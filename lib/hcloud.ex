de0695fe9f5b7c667528746c3c7e89d3a043a84b
Blackode <hi@blackode.in>
2018-02-18 02:15:38 +0530

    Actions API added

--- lib/hcloud.ex
+++ lib/hcloud.ex
@@ -1,9 +1,20 @@
 defmodule Hcloud do
-  def servers(), do: Hcloud.Client.get("servers")
+
+  alias Hcloud.Client
+
+  #Actions 
+  def actions() do 
+    Client.get("actions") 
+  end
+
+  def actions(id), do: Client.get("actions/" <> id)
+
+
+  def servers(), do: Client.get("servers")
 
   #Floating IPs
-  def floating_ips(), do: Hcloud.Client.get("floating_ips")
-  def floating_ips(id), do: Hcloud.Client.get("floating_ips/" <> id)
+  def floating_ips(), do: Client.get("floating_ips")
+  def floating_ips(id), do: Client.get("floating_ips/" <> id)
   @doc """
   Creates a new Floating IP assigned to a server. If you want to create a Floating IP that is not bound to a server, you need to provide the home_location key instead of server. This can be either the ID or the name of the location this IP shall be created in. Note that a Floating IP can be assigned to a server in any location later on. For optimal routing it is advised to use the Floating IP in the same Location it was created in.
 
@@ -20,57 +31,57 @@ defmodule Hcloud do
 
 
   """
-  def create_floating_ip( %{type: _ } = data), do: Hcloud.Client.get("floating_ips")
+  def create_floating_ip( %{type: _ } = data), do: Client.get("floating_ips")
   def update_floating_ip_description(id, description \\ nil) do
     Client.put("floating_ips/" <> id, description)
   end
-  def delete_floating_ip(id), do: Hcloud.Client.delete("floating_ips/" <> id)
+  def delete_floating_ip(id), do: Client.delete("floating_ips/" <> id)
 
 
   #ssh-keys
-  def ssh_keys(), do: Hcloud.Client.get("ssh_keys")
+  def ssh_keys(), do: Client.get("ssh_keys")
 
-  def ssh_key(id), do: Hcloud.Client.get("ssh_keys/#{id}")
+  def ssh_key(id), do: Client.get("ssh_keys/#{id}")
 
   def create_ssh_key(name, primary_key) do
-    Hcloud.Client.post("ssh_keys", %{name: name, primary_key: primary_key})
+    Client.post("ssh_keys", %{name: name, primary_key: primary_key})
   end
 
   def update_ssh_key(id, new_name) do
-    Hcloud.Client.put("ssh_keys/#{id}", %{name: new_name})
+    Client.put("ssh_keys/#{id}", %{name: new_name})
   end
 
-  def delete_ssh_key(id), do: Hcloud.Client.delete("ssh_keys/#{id}")
+  def delete_ssh_key(id), do: Client.delete("ssh_keys/#{id}")
 
   #server types
-  def server_types(), do: Hcloud.Client.get("server_types")
+  def server_types(), do: Client.get("server_types")
 
-  def server_types(id), do: Hcloud.Client.get("server_types/#{id}")
+  def server_types(id), do: Client.get("server_types/#{id}")
 
   #locations
-  def locations(), do: Hcloud.Client.get("locations")
+  def locations(), do: Client.get("locations")
 
-  def locations(id), do: Hcloud.Client.get("locations/#{id}")
+  def locations(id), do: Client.get("locations/#{id}")
 
   #datacenters
-  def data_centers(), do: Hcloud.Client.get("datacenters")
+  def data_centers(), do: Client.get("datacenters")
 
-  def data_centers(id), do: Hcloud.Client.get("datacenters/#{id}")
+  def data_centers(id), do: Client.get("datacenters/#{id}")
 
   #images
-  def images(), do: Hcloud.Client.get("images")
+  def images(), do: Client.get("images")
 
-  def images(id), do: Hcloud.Client.get("images/#{id}")
+  def images(id), do: Client.get("images/#{id}")
 
   def udpate_image(id, description, type) do
-    Hcloud.Client.put("images/#{id}", %{description: description, type: type })
+    Client.put("images/#{id}", %{description: description, type: type })
   end
 
-  def delete_image(id), do: Hcloud.Client.delete("images/#{id}")
+  def delete_image(id), do: Client.delete("images/#{id}")
 
-  def isos(), do: Hcloud.Client.get("isos")
+  def isos(), do: Client.get("isos")
 
-  def isos(id), do: Hcloud.Client.get("isos/#{id}")
+  def isos(id), do: Client.get("isos/#{id}")
 
-  def pricing(), do: Hcloud.Client.get("pricing")
+  def pricing(), do: Client.get("pricing")
 end
